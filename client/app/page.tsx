8d99d3c18fe9777381adb150fa858bb3b2718075
mabuhanifa <moh.abuhanifa@gmail.com>
2025-08-12 10:05:34 +0600

    shadcn added

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="bg-red-500">
-      <h1 className="text-3xl font-bold underline">Welcome to Yupsis App</h1>
-      <p className="mt-4 text-lg">
-        This is a sample application using Next.js and Tailwind CSS.
-      </p>
+    <div>
+      <Button>Click me</Button>
     </div>
   );
 }
